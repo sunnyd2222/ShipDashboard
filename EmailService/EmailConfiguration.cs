3d6f1bea9acf5c1a5b4f6bc15e737c67f990dbb7
sunnyd2222 <39214451+sunnyd2222@users.noreply.github.com>
2022-07-04 07:51:12 +0200

    EmailService

---
+++ EmailService/EmailConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MimeKit;
+namespace EmailService
+{
+    public class EmailConfiguration
+    {
+        public string From { get; set; }
+        public List<string> ToAdressList { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ EmailService/EmailSender.cs
@@ -0,0 +1,111 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfig;
+
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+        public List<MailboxAddress> To { get; set; }
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage);
+        }
+
+        public async Task SendEmailAsync(Message message)
+        {
+            var mailMessage = CreateEmailMessage(message);
+
+            await SendAsync(mailMessage);
+        }        
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            List<string> ToAdressJson = new List<string> { };
+            ToAdressJson = (_emailConfig.ToAdressList);
+            
+            IEnumerable<string> to = (ToAdressJson);
+            To = new List<MailboxAddress>();
+            To.AddRange(to.Select(x => new MailboxAddress(x)));     
+
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfig.From));
+            emailMessage.To.AddRange(To);
+            emailMessage.Subject = message.Subject;
+
+            var bodyBuilder = new BodyBuilder { HtmlBody = string.Format("<h3 style='color:black;'>{0}</h3>", message.Content) };
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                byte[] fileBytes;
+                foreach (var attachment in message.Attachments)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        attachment.CopyTo(ms);
+                        fileBytes = ms.ToArray();
+                    }
+
+                    bodyBuilder.Attachments.Add(attachment.FileName, fileBytes, ContentType.Parse(attachment.ContentType));
+                }
+            }
+
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer);
+                    client.Send(mailMessage);
+                }
+                catch
+                {
+                    //log an error message or throw an exception, or both.
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_emailConfig.SmtpServer);
+                    await client.SendAsync(mailMessage);
+                }
+                catch
+                {
+                    //log an error message or throw an exception, or both.
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
